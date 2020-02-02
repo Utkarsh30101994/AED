a199ef8f6257e022eafc361d347814fae6ab2bff
Utkarsh Kakkar <utkarsh.kakkar94@gmail.com>
2020-02-02 00:07:41 -0500

    vital signs

---
+++ Assignment1/src/Details/Profile.java
@@ -0,0 +1,303 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Details;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Utkarsh
+ */
+public class Profile {
+   private String firstName;
+   private String lastName;
+   private String phoneNum;
+   private String dob;
+   private String age;
+   private String height;
+   private String weight;
+   private String ssn; 
+   private String streetAddress;
+   private String city;
+   private String state;
+   private String zipCode;
+   private String bankName;
+   private String bankRoutingNum;
+   private String bankAccountNum;
+   private String accountBal;
+   private String accountType;
+   private String bankName1;
+   private String bankRoutingNum1;
+   private String bankAccountNum1;
+   private String accountBal1;
+   private String accountType1;
+   private String License_Num;
+   private String dateOfIssue;
+   private String dateOfExpiry;
+   private String bloodType;
+   private String ImageIcon;
+   private String medicalRecNum;
+   private String alergy1;
+   private String alergy2;
+   private String alergy3;
+      private ImageIcon Img;
+
+    public ImageIcon getImg() {
+        return Img;
+    }
+
+    public void setImg(ImageIcon Img) {
+        this.Img = Img;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankRoutingNum() {
+        return bankRoutingNum;
+    }
+
+    public void setBankRoutingNum(String bankRoutingNum) {
+        this.bankRoutingNum = bankRoutingNum;
+    }
+
+    public String getBankAccountNum() {
+        return bankAccountNum;
+    }
+
+    public void setBankAccountNum(String bankAccountNum) {
+        this.bankAccountNum = bankAccountNum;
+    }
+
+    public String getAccountBal() {
+        return accountBal;
+    }
+
+    public void setAccountBal(String accountBal) {
+        this.accountBal = accountBal;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBankName1() {
+        return bankName1;
+    }
+
+    public void setBankName1(String bankName1) {
+        this.bankName1 = bankName1;
+    }
+
+    public String getBankRoutingNum1() {
+        return bankRoutingNum1;
+    }
+
+    public void setBankRoutingNum1(String bankRoutingNum1) {
+        this.bankRoutingNum1 = bankRoutingNum1;
+    }
+
+    public String getBankAccountNum1() {
+        return bankAccountNum1;
+    }
+
+    public void setBankAccountNum1(String bankAccountNum1) {
+        this.bankAccountNum1 = bankAccountNum1;
+    }
+
+    public String getAccountBal1() {
+        return accountBal1;
+    }
+
+    public void setAccountBal1(String accountBal1) {
+        this.accountBal1 = accountBal1;
+    }
+
+    public String getAccountType1() {
+        return accountType1;
+    }
+
+    public void setAccountType1(String accountType1) {
+        this.accountType1 = accountType1;
+    }
+
+    public String getLicense_Num() {
+        return License_Num;
+    }
+
+    public void setLicense_Num(String License_Num) {
+        this.License_Num = License_Num;
+    }
+
+    public String getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(String dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public String getDateOfExpiry() {
+        return dateOfExpiry;
+    }
+
+    public void setDateOfExpiry(String dateOfExpiry) {
+        this.dateOfExpiry = dateOfExpiry;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getImageIcon() {
+        return ImageIcon;
+    }
+
+    public void setUpload(String ImageIcon) {
+        this.ImageIcon = ImageIcon;
+    }
+
+    public String getMedicalRecNum() {
+        return medicalRecNum;
+    }
+
+    public void setMedicalRecNum(String medicalRecNum) {
+        this.medicalRecNum = medicalRecNum;
+    }
+
+    public String getAlergy1() {
+        return alergy1;
+    }
+
+    public void setAlergy1(String alergy1) {
+        this.alergy1 = alergy1;
+    }
+
+    public String getAlergy2() {
+        return alergy2;
+    }
+
+    public void setAlergy2(String alergy2) {
+        this.alergy2 = alergy2;
+    }
+
+    public String getAlergy3() {
+        return alergy3;
+    }
+
+    public void setAlergy3(String alergy3) {
+        this.alergy3 = alergy3;
+    }
+}
